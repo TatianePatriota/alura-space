b44e37e3705deb819863cbab16a49a9d74bd27dc
Tatiane Patriota <tatianepatriota1@gmail.com>
2022-12-09 10:48:41 -0300

    add estilos ao componente

--- src/Components/HomePage/index.jsx
+++ src/Components/HomePage/index.jsx
@@ -1,7 +1,51 @@
+import styled from "styled-components";
 import Header from "../Header";
+import Sidebar from "../Sidebar";
+import banner from "../../assets/banner.png";
+
+const StyledMain = styled.main`
+  margin: 0 24px;
+`;
+
+const StyledContainerMain = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 2rem;
+`;
+
+const StyledTitle = styled.h1`
+  position: absolute;
+  z-index: 1;
+  color: #d9d9d9;
+  font-size: 2rem;
+  width: 275px;
+  height: 144px;
+  top: 186px;
+  left: 265px;
+`;
+
+const StyledImage = styled.img`
+  width: 88%;
+  height: 288px;
+`;
 
 function HomePage() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <StyledMain>
+        <StyledContainerMain>
+          <Sidebar />
+          <StyledTitle>A galeria mais completa de fotos do espaço!</StyledTitle>
+          <StyledImage
+            src={banner}
+            alt="Imagem da Terra vista do espaço"
+          ></StyledImage>
+        </StyledContainerMain>
+      </StyledMain>
+    </>
+  );
 }
 
 export default HomePage;
