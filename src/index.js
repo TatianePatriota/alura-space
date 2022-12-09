35e158d58417bae94b9c16155d49a6c307e416f9
Tatiane Patriota <tatianepatriota1@gmail.com>
2022-12-09 09:49:03 -0300

    add estilo global

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createGlobalStyle } from "styled-components";
+
+const StyledGlobal = createGlobalStyle`
+body {
+  margin: 0;
+  padding: 0;
+  min-height: 100vh;
+  background-image: linear-gradient(#041833, #154580);
+  box-sizing: border-box;
+}
+`;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <StyledGlobal />
     <App />
   </React.StrictMode>
 );
