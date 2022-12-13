c5f6f5e6e1c4cc22d031b7dfd9089c6960042b39
Tatiane Patriota <tatianepatriota1@gmail.com>
2022-12-13 14:00:06 -0300

    add theme

---
+++ src/Themes.js
@@ -0,0 +1,12 @@
+export const theme = {
+  colors: {
+    darkBlue: "#041833",
+    lightBlue: "#154580",
+    blueFooter: "#04244f",
+    pink: "#c98cf1",
+    purple: "#7b78e5",
+    lightGray: "#9d9d9d",
+    textColor: " #d9d9d9",
+    tagColor: "rgba(148, 163, 184, 0.3)",
+  },
+};
