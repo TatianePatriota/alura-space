0dfb2e5de2173fbf20dd51f0e922977a960bbebb
Tatiane Patriota <tatianepatriota1@gmail.com>
2022-12-14 08:46:10 -0300

    add componente da galeria

---
+++ src/Components/Galery/index.jsx
@@ -0,0 +1,13 @@
+import Tags from "../Tags";
+
+function Galery() {
+  return (
+    <section>
+      <h2>Navegue pela galeria</h2>
+      <Tags />
+      <ul></ul>
+    </section>
+  );
+}
+
+export default Galery;
