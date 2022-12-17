68b5c235b420e6f464fd8025a2450496c10313f7
Tatiane Patriota <tatianepatriota1@gmail.com>
2022-12-17 20:42:02 -0300

    add componente de fotos populares

---
+++ src/Components/Popular/index.jsx
@@ -0,0 +1,12 @@
+// import popularphotos from "./pupular-fotos.json";
+
+function Popular() {
+  return (
+    <aside>
+      <h2>Populares</h2>
+      <ul></ul>
+    </aside>
+  );
+}
+
+export default Popular;
